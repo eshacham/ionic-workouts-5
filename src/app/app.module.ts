05c5f48fd66cab2222d218862ed5659987da38f5
Eyal Shacham <eyal.shacham@emerson.com>
2019-12-28 12:58:14 -0600

    add all ios cordova deps

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,11 +8,19 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, StoreModule.forRoot(reducers, {
+      metaReducers,
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    })],
   providers: [
     StatusBar,
     SplashScreen,
