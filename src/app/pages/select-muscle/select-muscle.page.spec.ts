096e07a4a575934307511f6a08630330618c010b
Eyal Shacham <eyal.shacham@emerson.com>
2020-06-14 17:23:54 -0500

    added failing test

--- src/app/pages/select-muscle/select-muscle.page.spec.ts
+++ src/app/pages/select-muscle/select-muscle.page.spec.ts
@@ -1,16 +1,34 @@
 import { CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { Store } from '@ngrx/store';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Observable, Subscription } from 'rxjs';
+
 import { SelectMusclePage } from './select-muscle.page';
 
 describe('SelectMusclePage', () => {
   let component: SelectMusclePage;
   let fixture: ComponentFixture<SelectMusclePage>;
 
+  let storeSpy;
+  let routeSpy, routerSpy, routeSubscribeSpy, routeUnsubscribeSpy;
+
   beforeEach(async(() => {
+    routeSubscribeSpy = jasmine.createSpyObj('Observable', ['subscribe']);
+    routeUnsubscribeSpy = jasmine.createSpyObj('Subscription', ['unsubscribe']);
+    routeSpy = jasmine.createSpyObj('ActivatedRoute', [], { queryParams: routeSubscribeSpy});
+    // routerSpy = jasmine.createSpyObj()
+    storeSpy = jasmine.createSpyObj('Store', ['dispatch', 'select'])
+
     TestBed.configureTestingModule({
       declarations: [ SelectMusclePage ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
+      providers: [
+        { provide: ActivatedRoute, useValue: routeSpy },
+        { provide: Router, useValue: routerSpy },
+        { provide: Store, useValue: storeSpy },
+      ]
     })
     .compileComponents();
   }));
